2e5374b9a742293c2f8b73663e1bf1d7b8b0ccb4
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-08-13 11:32:42 -0300

    exercicio19: média + situação

---
+++ exerciciosFunctions/exercicio19.js
@@ -0,0 +1,21 @@
+// Nível 5 — Funções trabalhando juntas 🔥
+// 19. Média + situação
+
+function calcularMedia(nota1, nota2) {
+    return (nota1 + nota2) / 2;
+}
+
+function verificarSituacao(media) {
+
+    if (media >= 6) {
+        return "Aprovado";
+    } else {
+        return "Reprovado";
+    }
+}
+
+let media = calcularMedia(8, 7);
+let situacao = verificarSituacao(media);
+
+console.log(`Média: ${media}`);
+console.log(`Situação: ${situacao}`);
