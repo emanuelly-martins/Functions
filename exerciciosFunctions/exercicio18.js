54149681525085d5120f3f9c0cee71e76f4090d5
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-08-13 11:27:59 -0300

    exercicio18: criar mensagem aluno

---
+++ exerciciosFunctions/exercicio18.js
@@ -0,0 +1,8 @@
+// Nível 4 — Situações mais próximas de sistemas
+// 18. Criar mensagem de aluno
+
+function criarMensagem(nome, curso) {
+    console.log(`Olá, ${nome}! Você está matriculada no curso de ${curso}.`);
+}
+
+criarMensagem ("Emanuelly", "Desenvolvimento de Sistemas");
