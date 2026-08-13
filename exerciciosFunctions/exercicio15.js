d57de26ace29ba2a2449562c785c2640d40b9cdd
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-08-13 11:15:33 -0300

    exercicio15: classificação da nota

---
+++ exerciciosFunctions/exercicio15.js
@@ -0,0 +1,18 @@
+// Nível 3 — Funções + decisões
+// 15. Classificação da nota
+
+function classificarNota(nota) {
+
+    if (nota >= 9) {
+        return "Excelente";
+    } else if (nota >= 7) {
+        return "Bom";
+    } else if (nota === 6 ) {
+        return "Aprovado";
+    } else {
+        return "Reprovado";
+    }
+}
+
+let resultado = classificarNota(9);
+console.log(resultado);
