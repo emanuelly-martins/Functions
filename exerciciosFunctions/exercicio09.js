475c7ffa8c0dd46bc0cac3be8bf8fddf93f42536
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-08-13 10:30:16 -0300

    exercicio9: conversão de temperatura

---
+++ exerciciosFunctions/exercicio09.js
@@ -0,0 +1,8 @@
+// Nível 2 — Funções com cálculos
+// 9. Conversão de temperatura
+
+function celsisusParaFahrenheit(celsius) {
+    let f = ((celsius * 9) /5 + 32);
+    console.log(f)
+}
+celsisusParaFahrenheit(20)
